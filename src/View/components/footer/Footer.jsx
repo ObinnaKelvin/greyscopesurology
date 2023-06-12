35198a33f4b3b50bd96afc7c2cad895cc40b2ac1
Obinna Okere <okereobinna10@gmail.com>
2023-06-12 23:43:48 +0100

    Footer added 100%

--- src/View/components/footer/Footer.jsx
+++ src/View/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import icon from '../../assets/images/greyscopes_brand_allwhite.png'
 import './footer.scss'
 // import coverImage from "../../assets/images/direction.png"
@@ -9,24 +10,27 @@ import youtube from "../../assets/images/youtube_wh.png"
 // import appoint from "../../assets/images/appointment-book.png"
 
 const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+        
   return (
     <div>
         <div className="footer-container">
             <div className="footer-wrapper">
                 <div className="footer-col">
                     <img src={icon} alt="" />
-                    <div className="logo-text">...we are always available</div>
+                    {/* <div className="logo-text">...we are always available</div> */}
                 </div>
                 <div className="footer-col">
                     <div className="footer-col-header-text">
                         SITE MAP
                     </div>
                     <div className="footer-col-sub-text">
-                        <span>About Us</span>
-                        <span>Blog</span>
-                        <span>Gallery</span>
-                        <span>Contact Us</span>
-                        <span>Book an appointment</span>
+                        <Link to='/about' className='link'><span>About Us</span></Link>
+                        <Link to='/blog' className='link'><span>Blog</span></Link>
+                        <Link to='/gallery' className='link'><span>Gallery</span></Link>
+                        <Link to='/contact' className='link'><span>Contact Us</span></Link>
+                        <Link to='/book-appointment' className='link'><span>Book an appointment</span></Link>
                     </div>
 
                 </div>
@@ -58,7 +62,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="footer-tail">Copyright 2023 © All rights reserved</div>
+            <div className="footer-tail">Copyright {currentYear} © All rights reserved</div>
         </div>
     </div>
   )
